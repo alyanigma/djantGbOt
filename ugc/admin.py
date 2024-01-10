85eb793aa85b1145f6e2ddd7895d472a10e41583
alyanigma <alyanov1312@gmail.com>
2024-01-10 20:18:40 +0700

    y

--- ugc/admin.py
+++ ugc/admin.py
@@ -31,12 +31,12 @@ class ProfileAdmin(admin.ModelAdmin):
     def show_message(self, request, queryset):
         bot_token = TOKEN  
         bot = telebot.TeleBot(bot_token)
-        message_text = "Здравствуйте! Вы недавно проходили анкетирование, наш бот обновился, можете пройти тест заново."
+        message_text = "Привет, советуем прочитать данную статью о выборе факультета для поступления) https://vuzopedia.ru/region/city/69/poege"
         for message in queryset:
             chat_id = message.profile.external_id
             bot.send_message(chat_id, message_text)
 
         self.message_user(request, "Рассылка запущена")
-    show_message.short_description = "Создать рассылку"
+    show_message.short_description = "Запустить рассылку"
 
     actions = [show_message]
